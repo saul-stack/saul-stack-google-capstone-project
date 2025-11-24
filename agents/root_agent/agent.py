be325afe3a038b76b9aee29329e634fc797596c1
Saul <125879463+saul-stack@users.noreply.github.com>
2025-11-24 21:36:28 +0000

    feat: update root_agent to read Google Calendar

--- agents/root_agent/agent.py
+++ agents/root_agent/agent.py
@@ -1,67 +1,18 @@
-import datetime
-from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
+from google.adk.tools import FunctionTool
 
-def get_weather(city: str) -> dict:
-    """Retrieves the current weather report for a specified city.
+from .calendar_tools import get_schedule
 
-    Args:
-        city (str): The name of the city for which to retrieve the weather report.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-    if city.lower() == "new york":
-        return {
-            "status": "success",
-            "report": (
-                "The weather in New York is sunny with a temperature of 25 degrees"
-                " Celsius (77 degrees Fahrenheit)."
-            ),
-        }
-    else:
-        return {
-            "status": "error",
-            "error_message": f"Weather information for '{city}' is not available.",
-        }
-
-
-def get_current_time(city: str) -> dict:
-    """Returns the current time in a specified city.
-
-    Args:
-        city (str): The name of the city for which to retrieve the current time.
-
-    Returns:
-        dict: status and result or error msg.
-    """
-
-    if city.lower() == "new york":
-        tz_identifier = "America/New_York"
-    else:
-        return {
-            "status": "error",
-            "error_message": (
-                f"Sorry, I don't have timezone information for {city}."
-            ),
-        }
-
-    tz = ZoneInfo(tz_identifier)
-    now = datetime.datetime.now(tz)
-    report = (
-        f'The current time in {city} is {now.strftime("%Y-%m-%d %H:%M:%S %Z%z")}'
-    )
-    return {"status": "success", "report": report}
-
+get_schedule = FunctionTool(get_schedule)
 
 root_agent = Agent(
-    name="weather_time_agent",
+    name="calendar_agent",
     model="gemini-2.0-flash",
     description=(
-        "Agent to answer questions about the time and weather in a city."
+        "Agent to manage the user's calendar"
     ),
     instruction=(
-        "You are a helpful agent who can answer user questions about the time and weather in a city."
+        "You are a helpful agent, who will help the user to manage their timetable. You can answer questions about the user's upcoming schedule and events."
     ),
-    tools=[get_weather, get_current_time],
+    tools=[get_schedule],
 )
