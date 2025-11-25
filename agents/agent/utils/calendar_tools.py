0f8c66f119c6836758289ad764a9b440e220ff91
Saul <125879463+saul-stack@users.noreply.github.com>
2025-11-25 15:22:55 +0000

    feat: add max_results argument to get_events function

--- agents/agent/utils/calendar_tools.py
+++ agents/agent/utils/calendar_tools.py
@@ -3,8 +3,15 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from .handle_credentials import get_creds
 
-def get_events() -> dict:
+def get_events(max_results: int=10) -> dict:
     """Gets the upcoming events in the calendar
+
+    Args:
+        max_results: the number of events to fetch.
+
+    Returns:
+        A dictionary containing upcoming events and success message.
+    """
 
     try:
         creds = get_creds()
@@ -13,14 +20,13 @@ def get_events() -> dict:
 
     try:
         service = build("calendar", "v3", credentials=creds)
-
         now = datetime.datetime.utcnow().isoformat() + "Z"
         events_result = (
             service.events()
             .list(
                 calendarId="primary",
                 timeMin=now,
-                maxResults=10,
+                maxResults= max_results,
                 singleEvents=True,
                 orderBy="startTime",
             )
