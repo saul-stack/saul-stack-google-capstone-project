88a6ab40f207d3c9c552aa84ffdac7f02e5b386d
Saul <125879463+saul-stack@users.noreply.github.com>
2025-11-25 11:19:08 +0000

    feat: initialise hierarchical multi-agent structure

--- agents/agent/agent.py
+++ agents/agent/agent.py
@@ -5,14 +5,27 @@ from .utils.calendar_tools import get_schedule
 
 get_schedule = FunctionTool(get_schedule)
 
+
+calendar_interaction_agent = Agent(
+    name="calendar_interaction_agent",
+    description="Interact with the user's calendar",
+    tools=[get_schedule]
+)
+
+calendar_agent_team = Agent(
+    name="calendar_agent_team",
+    description="Manage the user's calendar and events",
+    sub_agents=[calendar_interaction_agent],
+)
+
 root_agent = Agent(
-    name="calendar_agent",
+    name="personal_assistant_agent",
     model="gemini-2.0-flash",
     description=(
-        "Agent to manage the user's calendar"
+        "Personal assistant agent"
     ),
     instruction=(
-        "You are a helpful agent, who will help the user to manage their timetable. You can answer questions about the user's upcoming schedule and events."
+        "You are a helpful assistant, who will help the user to manage their timetable and plan events."
     ),
-    tools=[get_schedule],
+    sub_agents=[calendar_agent_team],
 )
