f878b94c576e94a3093ee5936d2e12d29f2c3854
Saul <125879463+saul-stack@users.noreply.github.com>
2025-11-26 17:40:28 +0000

    dev: update get_events function and tool definitions

--- agents/agent/utils/calendar_tools.py
+++ agents/agent/utils/calendar_tools.py
@@ -1,13 +1,15 @@
-import datetime
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from .handle_credentials import get_creds
+from .math_and_time_tools import format_time_to_calendar, get_current_date_and_time
 
-def get_events(max_results: int=10) -> dict:
+def get_events(start_time = None, end_time = None, max_results: int=10) -> dict:
+
     """Gets the upcoming events in the calendar
-
     Args:
-        max_results (int): the max number of events to fetch.
+        max_results (int) - optional: the max number of events to fetch.
+        start_time - optional: the starting bounds for events to fetch
+        end_time - optional: the ending bounds for events to fetch
 
     Returns:
         A dictionary containing upcoming events and success message.
@@ -20,16 +22,28 @@ def get_events(max_results: int=10) -> dict:
 
     try:
         service = build("calendar", "v3", credentials=creds)
-        now = datetime.datetime.utcnow().isoformat() + "Z"
+
+        if start_time == None:
+            start_time = get_current_date_and_time()
+            start_time = format_time_to_calendar(start_time["time_iso"])
+
+        else:
+            start_time = format_time_to_calendar(start_time)  
+
+        params = {
+        "calendarId":"primary",
+        "timeMin":start_time,
+        "maxResults": max_results,
+        "singleEvents":True,
+        "orderBy":"startTime",
+        }
+
+        if end_time is not None:
+            params["timeMax"] = format_time_to_calendar(end_time)   
+
         events_result = (
             service.events()
-            .list(
-                calendarId="primary",
-                timeMin=now,
-                maxResults= max_results,
-                singleEvents=True,
-                orderBy="startTime",
-            )
+            .list(**params)
             .execute()
         )
         events = events_result.get("items", [])
